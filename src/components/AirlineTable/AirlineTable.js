76c86d79fb25a0be20632918777eaaabc4abf675
Kevin Anderson <kmichael419@hotmail.com>
2020-07-22 09:18:31 -0500

    adding material ui table

--- src/components/AirlineTable/AirlineTable.js
+++ src/components/AirlineTable/AirlineTable.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-//
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 class AirlineTable extends Component {
   render() {
     console.log('Table', this.props.reduxState);
     return (
-      <table>
-        <tbody>
-          <tr>
-            <th>Airline Name</th>
-            <th>Airline Number of Planes</th>
-          </tr>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Airline Name</TableCell>
+              <TableCell>Airline Number of Planes</TableCell>
+            </TableRow>
+          </TableHead>
 
           {this.props.reduxState.airlineList.map((item, index) => {
             return (
-              <tr key={index}>
-                <td>{item.enteredAirline}</td>
-                <td>{item.enteredAirlineNumber}</td>
-              </tr>
+              <TableBody>
+                <TableRow key={index}>
+                  <TableCell>{item.enteredAirline}</TableCell>
+                  <TableCell>{item.enteredAirlineNumber}</TableCell>
+                </TableRow>
+              </TableBody>
             );
           })}
-        </tbody>
-      </table>
+        </Table>
+      </TableContainer>
     );
   }
 }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import AirlineForm from '../AirlineForm/AirlineForm';
 import AirlineTable from '../AirlineTable/AirlineTable';
+import 'fontsource-roboto';
 
 class App extends Component {
   render() {
